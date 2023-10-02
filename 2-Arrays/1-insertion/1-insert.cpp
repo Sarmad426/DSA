e289bb314ec2515ad8dadddcba8851ff86dfc7ac
Sarmad rafique <sarmadrafique040@gmail.com>
2023-10-02 11:48:27 +0500

    renamed the variable for understanding

--- 2-Arrays/1-insertion/1-insert.cpp
+++ 2-Arrays/1-insertion/1-insert.cpp
@@ -8,12 +8,12 @@ int main()
 {
     int arr[5] = {1, 3, 5, 7, 8};
     int item = 10, index = 3, length = 5;
-    int i = 0, j = length;
+    int i = 0, upper_bound = length;
     length = length + 1;
-    while (j >= index)
+    while (upper_bound >= index)
     {
-        arr[j + 1] = arr[j];
-        j -= 1;
+        arr[upper_bound + 1] = arr[upper_bound];
+        upper_bound -= 1;
     }
     arr[index] = item;
     cout << "[ ";
