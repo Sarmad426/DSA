ae0d4b75f7664afa8ff0d1f8220709ffd333e0c1
Sarmad rafique <sarmadrafique040@gmail.com>
2023-10-25 09:50:43 +0500

    added run-time user input

--- 5-Search/linear-search.cpp
+++ 5-Search/linear-search.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
-int linear_search(int arr[], int n, int target) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == target) {
-            return i; 
+int linear_search(int arr[], int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
         }
     }
     return 0;
 }
-int main() {
+int main()
+{
     int arr[] = {2, 4, 6, 8, 10, 12, 14};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int target = 8;
+    int target;
+    cout << "Enter an element to search: " << endl;
+    cin >> target;
     int result = linear_search(arr, n, target);
-    if (result != 0) {
-      	cout <<target<<" found at index " << result <<endl;
-    } else {
-        cout << "Element not found in the array." <<endl;
+    if (result != 0)
+    {
+        cout << target << " found at index " << result << endl;
+    }
+    else
+    {
+        cout << "Element not found in the array." << endl;
     }
 }
-
