c2e9bc41944c61bba0bcabcc309a0f5f7d48ffa4
Sarmad rafique <sarmadrafique040@gmail.com>
2023-10-25 09:13:24 +0500

    linear search c++ code

---
+++ 5-Search/linear-search.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int linear_search(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return 0;
+}
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
+    int result = linear_search(arr, n, target);
+    if (result != 0) {
+      	cout <<target<<" found at index " << result <<endl;
+    } else {
+        cout << "Element not found in the array." <<endl;
+    }
+}
+
