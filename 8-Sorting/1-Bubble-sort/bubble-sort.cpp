31012563f5d48ddadedc05282abe7c1537bb055c
Sarmad rafique <sarmadrafique040@gmail.com>
2023-12-06 09:58:44 +0500

    Bubble sort program using Object Oriented

---
+++ 8-Sorting/1-Bubble-sort/bubble-sort.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Bubble_Sort {
+    int size;
+    int *arr; 
+public:
+    Bubble_Sort() {
+        cout << "Input array Size: " << endl;
+        cin >> size;
+
+        // Dynamically allocate memory for the array
+        arr = new int[size];
+    }
+
+    ~Bubble_Sort() {
+        // Deallocate memory when the object is destroyed
+        delete[] arr;
+    }
+
+    void input_array() {
+        cout << "Input " << size << " values in array: " << endl;
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+            cout << endl;
+        }
+    }
+
+    void display_array() {
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << ", ";
+        }
+    }
+
+    void swap(int &num1, int &num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    void sort() {
+        bool swapped = true;
+
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < size - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr[i], arr[i + 1]);
+                    swapped = true;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Bubble_Sort array;
+    array.input_array();
+    array.sort();
+    array.display_array();
+}
+
