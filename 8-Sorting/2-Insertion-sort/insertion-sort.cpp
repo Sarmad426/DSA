c6eac83b06b571c6f917bab6908f16bdf5d926ff
Muhammad Sarmad <sarmadrafique040@gmail.com>
2023-12-06 19:36:20 +0500

    Minor syntax update.

--- 8-Sorting/2-Insertion-sort/insertion-sort.cpp
+++ 8-Sorting/2-Insertion-sort/insertion-sort.cpp
@@ -38,7 +38,7 @@ public:
             key = arr[i];
             j = i - 1;
 
-            while (j >= 0 && arr[j] > key) {
+            while (j>=0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
