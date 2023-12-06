443e6db1f3530d5879d386d56ad6574a77fca0d6
Sarmad rafique <sarmadrafique040@gmail.com>
2023-12-06 13:54:23 +0500

    selection sort program

---
+++ 8-Sorting/3-Selection-sort/selection-sort.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Selection_Sort {
+    int size;
+    int *arr; 
+public:
+    Selection_Sort() {
+        cout << "Input array Size: " << endl;
+        cin >> size;
+
+        // Dynamically allocate memory for the array
+        arr = new int[size];
+    }
+
+    ~Selection_Sort() {
+        delete[] arr;
+    }
+
+    void input_array() {
+        cout << "Input " << size << " values in array: " << endl;
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+            cout << endl;
+        }
+    }
+
+    void display_array() {
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << ", ";
+        }
+    }
+
+    void swap(int &num1, int &num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+};
+
+int main() {
+    Selection_Sort array;
+    array.input_array();
+    array.sort();
+    array.display_array();
+}
+
