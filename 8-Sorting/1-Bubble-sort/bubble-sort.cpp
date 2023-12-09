301e1b15e5c91ff9aa8d8a680bc7381b503768c9
Muhammad Sarmad <sarmadrafique040@gmail.com>
2023-12-09 16:42:06 +0500

    Updated the Bubble Sort algo

--- 8-Sorting/1-Bubble-sort/bubble-sort.cpp
+++ 8-Sorting/1-Bubble-sort/bubble-sort.cpp
@@ -39,18 +39,14 @@ public:
     }
 
     void sort() {
-        bool swapped = true;
-
-        while (swapped) {
-            swapped = false;
-            for (int i = 0; i < size - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr[i], arr[i + 1]);
-                    swapped = true;
-                }
-            }
-        }
-    }
+          for(int i=0;i<size;i++){
+        	for(int j=1;j<size;j++){
+         		if(arr[j] < arr[j-1]){
+         			swap(arr[j],arr[j-1]);
+        		}
+			}		   
+    	}
+	}
 };
 
 int main() {
